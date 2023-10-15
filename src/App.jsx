89f619fd7f14768ed2819db008c7c26d81ab9bdd
Nu <nuuseforstudy@gmail.com>
2023-10-15 15:59:49 +0700

    edit: scrollup

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Homepage from "./pages/Homepage";
 import Recentwork from "./pages/Recentwork";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ThemeProvider from "./Context/themeContext";
+import ScrollToTop from "./component/ScrollUp";
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/Recentworks" element={<Recentwork />} />
